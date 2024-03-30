175fb3d3a1d1b13f5e32d63103ec66dd9335d394
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-03-30 02:49:06 +0100

    JS- funkce na posun skill-barů a hodnot, výzva pro uživatele

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('funguju!');
+console.log("funguju!");
+
+const urovenHTML = Number(
+  prompt("Vyjádřete číslem na stupnici od 0 do 100 svou úroveň znalosti HTML:")
+);
+const urovenCSS = Number(
+  prompt("Vyjádřete číslem na stupnici od 0 do 100 svou úroveň znalosti CSS:")
+);
+const urovenJS = Number(
+  prompt(
+    "Vyjádřete číslem na stupnici od 0 do 100 svou úroveň znalosti JavaScriptu:"
+  )
+);
+
+const updateSkill = (id, skill) => {
+  const progressBar = document
+    .getElementById("skill" + id)
+    .querySelector(".skill__progress");
+  progressBar.style.width = skill + "%";
+  const skillValue = document
+    .getElementById("skill" + id)
+    .querySelector(".skill__value");
+  skillValue.textContent = `${skill} / 100`;
+};
+
+updateSkill(1, urovenHTML);
+updateSkill(2, urovenCSS);
+updateSkill(3, urovenJS);
